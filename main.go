569379a0ca53789b35f27e6f78f9e7df6ab06089
bharathcassiescardinals <viswas163@gmail.com>
2020-05-22 19:57:29 -0400

    Fetching API Key from user

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/viswas163/MarvelousShipt/routes"
 )
 
+// MarvelAPIKeyEnvKey : The OS environment key to fetch the Marvel Developer API Key string
+var MarvelAPIKeyEnvKey = "MARVEL_API_KEY"
+
 func main() {
+	hasKey := ""
 
 	fmt.Println("Starting Server...")
+
+	for !strings.EqualFold(hasKey, "y") && !strings.EqualFold(hasKey, "n") && !strings.EqualFold(hasKey, "yes") && !strings.EqualFold(hasKey, "no") {
+		fmt.Println("Do you have a Marvel Developer API Key? (y/n)")
+		fmt.Scanln(&hasKey)
+	}
+
+	if strings.EqualFold(hasKey, "y") || strings.EqualFold(hasKey, "yes") {
+		userAPIKey := ""
+		fmt.Println("Enter the API Key please...")
+		fmt.Scanln(&userAPIKey)
+		os.Setenv(MarvelAPIKeyEnvKey, userAPIKey)
+	}
 
 	routes.HandleRoutes()
 
