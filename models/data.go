d3cdb937ee86a2669dc4ed9734ebfb268dc83711
bharathcassiescardinals <viswas163@gmail.com>
2020-05-27 02:04:19 -0400

    Cleanup

--- models/data.go
+++ models/data.go
@@ -1,7 +1,6 @@
 package models
 
 // DataWrapper provides the common wrapper attributes to unmarshal the API's response.
-// It is used to compose more specific wrappers, e.g., CharacterDataWrapper.
 type DataWrapper struct {
 	Code            int    `json:"code,omitempty"`
 	Status          string `json:"status,omitempty"`
@@ -12,7 +11,6 @@ type DataWrapper struct {
 }
 
 // DataContainer provides the common container attributes to unmarshal the API's response.
-// It is used to compose more specific containers, e.g., CharacterDataContainer.
 type DataContainer struct {
 	Offset int `json:"offset,omitempty"`
 	Limit  int `json:"limit,omitempty"`
@@ -21,14 +19,12 @@ type DataContainer struct {
 }
 
 // Summary provides the common summary attributes to unmarshal the API's response.
-// It is used to compose more specific containers, e.g., CharacterSummary.
 type Summary struct {
 	ResourceURI string `json:"resourceURI,omitempty"`
 	Name        string `json:"name,omitempty"`
 }
 
 // List provides the common list attributes to unmarshal the API's response.
-// It is used to compose more specific containers, e.g., CharacterList.
 type List struct {
 	Available     int    `json:"available,omitempty"`
 	Returned      int    `json:"returned,omitempty"`
